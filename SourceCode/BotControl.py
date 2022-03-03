1b6de8babe1a398c546efde091043aafe73fe27d
Patrick O'Connor <oconnor.p2016@gmail.com>
2022-03-03 11:49:28 -0700

    Attempt at moving waist left

--- SourceCode/BotControl.py
+++ SourceCode/BotControl.py
@@ -6,6 +6,7 @@ import tkinter as tk
 
 TARGET_CENTER = 5896
 
+
 def getUSB():
     usb = None
     try:
@@ -22,17 +23,18 @@ def getUSB():
             sys.exit(0)
     return usb
 
+
 class RobotMotor(Enum):
-    WheelLeft=0x00
-    WheelRight=0x01
-    Waist=0x02
-    HeadX=0x03
-    HeadY=0x04
-    ArmLeft=0x05
-    ArmRight=0x06
+    WheelLeft = 0x00
+    WheelRight = 0x01
+    Waist = 0x02
+    HeadX = 0x03
+    HeadY = 0x04
+    ArmLeft = 0x05
+    ArmRight = 0x06
+
 
 class TangoRobot:
-
     # class properties
     usb = None
     win = None
@@ -49,17 +51,17 @@ class TangoRobot:
         # tkinter window
         self.win = tk.Tk()
         # setup keybindings
-        self.win.bind('<Up>', self.arrows)   # key code: 111
-        self.win.bind('<Down>', self.arrows) # key code: 116
-        self.win.bind('<Left>', self.arrows) # key code: 113
-        self.win.bind('<Right>', self.arrows) # key code: 114
-        self.win.bind('<space>', self.stop) # key code: 65
-        self.win.bind('<w>', self.head) # key code: 25
-        self.win.bind('<s>', self.head) # key code: 39
-        self.win.bind('<a>', self.head) # key code: 38
-        self.win.bind('<d>', self.head) # key code: 40
-        self.win.bind('<z>', self.waist) # key code: 52
-        self.win.bind('<c>', self.waist) # key code: 54
+        self.win.bind('<Up>', self.arrows)  # key code: 111
+        self.win.bind('<Down>', self.arrows)  # key code: 116
+        self.win.bind('<Left>', self.arrows)  # key code: 113
+        self.win.bind('<Right>', self.arrows)  # key code: 114
+        self.win.bind('<space>', self.stop)  # key code: 65
+        self.win.bind('<w>', self.head)  # key code: 25
+        self.win.bind('<s>', self.head)  # key code: 39
+        self.win.bind('<a>', self.head)  # key code: 38
+        self.win.bind('<d>', self.head)  # key code: 40
+        self.win.bind('<z>', self.waist)  # key code: 52
+        self.win.bind('<c>', self.waist)  # key code: 54
         # start tkinter window
         self.win.mainloop()
 
@@ -71,7 +73,7 @@ class TangoRobot:
             print("Motor must be type {!s} - type '{!s}' not accepted".format(type(RobotMotor), type(motor)))
             return
         # Build command
-        lsb = target &0x7F
+        lsb = target & 0x7F
         msb = (target >> 7) & 0x7F
         command = chr(0xaa) + chr(0xC) + chr(0x04) + chr(motor.value) + chr(lsb) + chr(msb)
         # Check if usb is not None
@@ -93,6 +95,7 @@ class TangoRobot:
     def waist(self, event):
         keycode = event.keycode
         if keycode == 52:
+            self.writeCmd(RobotMotor.Waist, 200)
             print("Z (Left)")
         elif keycode == 54:
             print("C (Right)")
@@ -100,15 +103,16 @@ class TangoRobot:
     def head(self, event):
         keycode = event.keycode
         if keycode == 25:
-            print("Head Up")
+            print("W: Head Up")
         elif keycode == 39:
-            print("Head Down")
+            print("S: Head Down")
         elif keycode == 38:
-            print("Head Left")
+            print("A: Head Left")
         elif keycode == 40:
-            print("Head Right")
+            print("D: Head Right")
 
     def stop(self, event=None):
         self.win.destroy()
+
 
 robot = TangoRobot()
