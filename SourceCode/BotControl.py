4d471b52160df7160ce28320b9d22ed9d480134d
Patrick O'Connor <oconnor.p2016@gmail.com>
2022-03-03 11:56:12 -0700

    Attempt at moving waist and head left and right

--- SourceCode/BotControl.py
+++ SourceCode/BotControl.py
@@ -5,6 +5,9 @@ from enum import Enum
 import tkinter as tk
 
 TARGET_CENTER = 5896
+MAX_SERVO = 7900
+MIN_SERVO = 4100
+
 
 
 def getUSB():
@@ -95,21 +98,45 @@ class TangoRobot:
     def waist(self, event):
         keycode = event.keycode
         if keycode == 52:
-            self.writeCmd(RobotMotor.Waist, 200)
             print("Z (Left)")
+            self.motors += 200
+            if(self.motors > MAX_SERVO):
+                self.motors = MAX_SERVO
+            self.Robot.setTarget(RobotMotor.Waist, self.motors)
         elif keycode == 54:
             print("C (Right)")
+            self.motors += 200
+            if(self.motors > MIN_SERVO):
+                self.motors = MIN_SERVO
+            self.Robot.setTarget(RobotMotor.Waist, self.motors)
 
     def head(self, event):
         keycode = event.keycode
         if keycode == 25:
             print("W: Head Up")
+            self.motors += 200
+            if(self.motors > MAX_SERVO):
+                self.motors = MAX_SERVO
+            self.Robot.setTarget(RobotMotor.HeadY, self.motors)
+
         elif keycode == 39:
             print("S: Head Down")
+            self.motors -= 200
+            if(self.motors > MIN_SERVO):
+                self.motors = MIN_SERVO
+            self.Robot.setTarget(RobotMotor.HeadY, self.motors)
         elif keycode == 38:
             print("A: Head Left")
+            self.motors += 200
+            if(self.motors > MAX_SERVO):
+                self.motors = MAX_SERVO
+            self.Robot.setTarget(RobotMotor.HeadX, self.motors)
         elif keycode == 40:
             print("D: Head Right")
+            self.motors -= 200
+            if(self.motors > MIN_SERVO):
+                self.motors = MIN_SERVO
+            self.Robot.setTarget(RobotMotor.HeadY, self.motors)
 
     def stop(self, event=None):
         self.win.destroy()
