209a1a3221125c07495ad5c696f54ce605e30b88
Patrick O'Connor <oconnor.p2016@gmail.com>
2022-03-03 11:35:13 -0700

    Fix minor spelling error

--- SourceCode/BotControl.py
+++ SourceCode/BotControl.py
@@ -45,7 +45,7 @@ class TangoRobot:
         # center robot head on X
         self.writeCmd(RobotMotor.HeadX, TARGET_CENTER)
         # center robot head on Y
-        self.writeCmd(RobotMotor.HeadX, TARGET_CENTER)
+        self.writeCmd(RobotMotor.HeadY, TARGET_CENTER)
         # tkinter window
         self.win = tk.Tk()
         # setup keybindings
