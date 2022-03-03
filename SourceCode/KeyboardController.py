ac0f588324544425984182e50da3209b815f3dd2
Patrick O'Connor <oconnor.p2016@gmail.com>
2022-03-03 14:19:54 -0700

    Refactor code into Robot class and a KeyboardController.py

--- SourceCode/KeyboardController.py
+++ SourceCode/KeyboardController.py
@@ -1,6 +1,24 @@
+from TangoRobot import *
 
 
-from SourceCode.TangoRobot import *
+class KeyboardController:
+    def __init__(self):
+        # tkinter window
+        self.win = tk.Tk()
+        # setup keybindings
+        self.win.bind('<Up>', self.arrows)  # key code: 111
+        self.win.bind('<Down>', self.arrows)  # key code: 116
+        self.win.bind('<Left>', self.arrows)  # key code: 113
+        self.win.bind('<Right>', self.arrows)  # key code: 114
+        self.win.bind('<space>', self.stop)  # key code: 65
+        self.win.bind('<w>', self.head)  # key code: 25
+        self.win.bind('<s>', self.head)  # key code: 39
+        self.win.bind('<a>', self.head)  # key code: 38
+        self.win.bind('<d>', self.head)  # key code: 40
+        self.win.bind('<z>', self.waist)  # key code: 52
+        self.win.bind('<c>', self.waist)  # key code: 54
+        # start tkinter window
+        self.win.mainloop()
 
 
 def arrows(self, event):
