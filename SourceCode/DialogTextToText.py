cfcdbd1bf16ad9dd1f5ddbeae503c96cd6325b44
Patrick O'Connor <oconnor.p2016@gmail.com>
2022-04-04 13:40:27 -0600

    init DialogTextToText.py and Human.py

---
+++ SourceCode/DialogTextToText.py
@@ -0,0 +1,16 @@
+from SourceCode.Human import Human
+from TangoRobot import *
+import time
+import Human
+
+
+class DialogTextToText:
+
+    human = Human()
+
+    def interpret(humanInput):
+        pass
+
+    humanInput = input("Human: ")
+    while humanInput != "bye":
+        interpret(humanInput)
---
+++ SourceCode/Human.py
@@ -0,0 +1,8 @@
+class Human:
+    # class properties
+    name = "I don't know"
+
+    # constructor
+    def __init__(self):
+        pass
+
