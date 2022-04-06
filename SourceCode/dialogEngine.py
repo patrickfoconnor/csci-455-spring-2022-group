a97b5b2c0df198b7e094ae10aba1becdc89885bb
Nick <dominiczumpanolivingston@gmail.com>
2022-04-06 17:07:49 -0600

    varibles done

--- SourceCode/dialogEngine.py
+++ SourceCode/dialogEngine.py
@@ -8,23 +8,41 @@ def command(commandString, lineNumber):
     robotOutputs = []
     variableName = ""
     for i in range(len(commandString)):
-        if commandString[i].isdigit():
-            level = commandString[i]
-        if commandString[i] == "(":
+        if commandString[i] == "u":
             i += 1
-            while commandString[i] != ")":
-                humanInput += commandString[i]
+        elif commandString[i].isdigit():
+            level = commandString[i]
+            i += 1
+        elif commandString[i] == "(":
+            i += 1
+            if commandString[i] == "~":
                 i += 1
-                if commandString[i] == "_":
-                    x = i
-                    variable = ""
-                    while commandString[x] != "$":
+                temp = ""
+                while commandString[i].isalpha():
+                    temp += commandString[i]
+                    i += 1
+                for vari in def_lists:
+                    print(vari[0])
+                    if vari[0] == temp:
+                        humanInput = vari[1]
+
+            else:
+                while commandString[i] != ")":
+                    humanInput += commandString[i]
+                    i += 1
+                    if commandString[i] == "_":
+                        x = i
+                        variable = "$"
+                        while commandString[x] != "$":
+                            x += 1
+                            test = commandString[x]
                         x += 1
                         while commandString[x].isalpha():
+                            print(commandString[x])
                             variable += commandString[x]
+                            x += 1
                         variableName = variable
-
-        if commandString[i] == ":":
+        elif commandString[i] == ":":
             i += 1
             if commandString[i] == "[":
                 while commandString[i] != "]":
@@ -48,13 +66,21 @@ def command(commandString, lineNumber):
                     i += 1
                 robotOutputs.append(robotOut)
             elif commandString[i] == "~":
-                print("scream")
+                i += 1
+                temp = ""
+                while commandString[i].isalpha():
+                    temp += commandString[i]
+                    i += 1
+                for vari in def_lists:
+                    print(vari[0])
+                    if vari[0] == temp:
+                        robotOutputs = vari[1]
     return level, humanInput, robotOutputs, variableName
 
 
 def definition(definitionString, lineNumber):
     variable = ""
-    variable_names = []
+    variable_names = ""
     variableOptions = []
     for i in range(0,len(definitionString)):
         if definitionString[i] == "~":
@@ -62,7 +88,7 @@ def definition(definitionString, lineNumber):
             while definitionString[i].isalpha():
                 variable += definitionString[i]
                 i += 1
-            variable_names.append(variable)
+            variable_names = variable
         if definitionString[i] == ":" and definitionString[i + 1] == "[":
             i += 1
             while definitionString[i] != "]":
