3fe296fb444ea0f81a3f31de8b43ec0219d9b3c6
Nick <dominiczumpanolivingston@gmail.com>
2022-04-07 12:02:10 -0600

    some work on scope completed

--- SourceCode/DialogTextToText.py
+++ SourceCode/DialogTextToText.py
@@ -1,10 +1,12 @@
 #from TangoRobot import *
 import time
 import dialogEngine
+import random
 #import speech_recognition as sr
 
 rulesList = dialogEngine.fileReader()
 listening = True;
+r = random.seed
 
 
 def listen():
@@ -29,15 +31,42 @@ def typing():
     return humanInput
 
 
-def interpret(self, humanInput):
-    pass
+def typeBack(out):
+    if isinstance(out, list):
+        print(random.choice(out))
+    else:
+        print(out)
+
+
+
 
 
 def main():
     level = 0
     i = 0
     if rulesList[0][0] == level:
-        while rulesList[i][0] < level:
+        humanRes = rulesList[0][1]
+        humanInput = typing()
+        if isinstance(humanRes, str):
+            if humanInput == humanRes:
+                typeBack(rulesList[0][2])
+
+        elif isinstance(humanRes, list):
+            for word in humanRes:
+                if humanInput == word:
+                    typeBack(rulesList[0][2])
+        level += 1
+        i += 1
+        x = i
+        currentList = []
+        while int(rulesList[i][0]) >= level:
+            if int(rulesList[x][0]) == level:
+                currentList.append(rulesList[x])
             i += 1
+            x += 1
+        humanInput = typing()
+        for j in range(0, len(currentList)):
+            if humanInput == currentList[j][1]:
+                typeBack(currentList[j][2])
 
 main()
