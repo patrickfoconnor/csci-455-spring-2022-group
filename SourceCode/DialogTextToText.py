153880d0abac2d7c56390294d5d63b37c0f2b180
Nick <dominiczumpanolivingston@gmail.com>
2022-04-09 17:48:28 -0600

    made it look pretty

--- SourceCode/DialogTextToText.py
+++ SourceCode/DialogTextToText.py
@@ -1,5 +1,4 @@
 #from TangoRobot import *
-import time
 import dialogEngine
 import random
 import sys
@@ -29,28 +28,29 @@ def listen():
 
 def typing():
     humanInput = input("Human: ")
-    if humanInput == "exit": # exits program
+    if humanInput == "exit":  # exits program
         sys.exit()
     return humanInput
 
 # Return the human data previously recorded
-def getHumanData(varName, dict):
-    if varName in dict.keys():
-        humanData = dict.get(varName)
+
+
+def getHumanData(varName, dicts):
+    if varName in dicts.keys():
+        humanData = dicts.get(varName)
     else:
         humanData = "I Dont Know"
     return humanData
 
 
-def typeBack(out, dict, varName):
+def typeBack(out, dicts, varName):
     # Thinking that checking here for the $ sign
-    if isinstance(out, list): # if response is list
+    if isinstance(out, list):  # if response is list
         output = ""
-        test = bool(out)
         if bool(out):
             output = random.choice(out)
         if "$" in output or not bool(out):
-            humanData = getHumanData(varName, dict)
+            humanData = getHumanData(varName, dicts)
             if humanData is None:
                 print(output.replace(varName, "I don't know").title())
             elif humanData != "":
@@ -60,13 +60,13 @@ def typeBack(out, dict, varName):
 
     else:
         if varName != "":
-            humanData = getHumanData(varName, dict)
+            humanData = getHumanData(varName, dicts)
             if humanData != "":
                 print(humanData.title())
             else:
                 print("I don't know")
         else:
-            print(out.title()) # if response is string
+            print(out.title())  # if response is string
 
 
 def checkForVariables(humanInput, humanRes):
@@ -89,7 +89,7 @@ def getVarName(out):
     if "$" in output:
         varName = "$"
         varFound = False
-        for i in range (0, len(output)):
+        for i in range(0, len(output)):
             if output[i] == "$":
                 varFound = True
 
@@ -101,32 +101,31 @@ def getVarName(out):
 
 
 def findDif(i, rules, humanRes, humanInput, humanDataDict, varName):
-        if humanInput == humanRes:
-            out = rules[i][2]
+    if humanInput == humanRes:
+        out = rules[i][2]
+        if varName == "":
+            varName = getVarName(rules[i][2])
+        typeBack(out, humanDataDict, varName)
+        return True
+    elif "_" in humanRes:
+        humanData = checkForVariables(humanInput, humanRes)
+        if humanData != "" and humanRes[:humanRes.index("_")] in humanInput:
             if varName == "":
                 varName = getVarName(rules[i][2])
-            typeBack(out, humanDataDict, varName)
+            humanDataDict[varName] = humanData
+            typeBack(rules[i][2], humanDataDict, varName)
             return True
-        elif "_" in humanRes:
-            humanData = checkForVariables(humanInput, humanRes)
-            if humanData != "" and humanRes[:humanRes.index("_")] in humanInput:
-                if varName == "":
-                    varName = getVarName(rules[i][2])
-                humanDataDict[varName] = humanData
-                typeBack(rules[i][2], humanDataDict, varName)
-                return True
 
 
 def main():
-    level = 0
     i = 0
     humanDataDict = {}
     humanInput = typing().lower()
     breaking = False
     varName = ""
-    while not breaking: # this while loop checks the top level of options
-        humanRes = rulesList[i][1] # what the robot is looking to respond to
-        if int(rulesList[i][0]) > 0: # if the level is higher that first level it skips the loop.
+    while not breaking:  # this while loop checks the top level of options
+        humanRes = rulesList[i][1]  # what the robot is looking to respond to
+        if int(rulesList[i][0]) > 0:  # if the level is higher that first level it skips the loop.
             pass
         if isinstance(humanRes, str):
             breaking = findDif(i, rulesList, humanRes, humanInput, humanDataDict, varName)
@@ -136,17 +135,17 @@ def main():
         i += 1
     level = 1
     nextList = []
-    currentLevel = 1 # currentLevel is the level we want to look at
-    while len(rulesList) > 0: # while there are still rules (might not be needed)
+    currentLevel = 1  # currentLevel is the level we want to look at
+    while len(rulesList) > 0:  # while there are still rules (might not be needed)
         currentList = nextList
         while currentLevel <= level and i < len(rulesList):
-            if int(rulesList[i][0]) == level: # places rules into the current list if it is the level we are looking at
+            if int(rulesList[i][0]) == level:  # places rule into the current list if it is the level we are looking at
                 currentList.append(rulesList[i])
             else:
-                if int(rulesList[i][0]) < currentLevel: # if nothing left on lower levels, leaves and goes to responses
+                if int(rulesList[i][0]) < currentLevel:  # if nothing left on lower levels, leaves and goes to responses
                     currentLevel += 1
                 else:
-                    nextList.append(rulesList[i]) # places rules into the list to look at next
+                    nextList.append(rulesList[i])  # places ruleList into the list to look at next
 
             i += 1
         j = 0
@@ -163,6 +162,8 @@ def main():
                         breaking = findDif(j, currentList, word, humanInput, humanDataDict, varName)
 
                 j += 1
+
+
 while True:
     print("")
     main()
