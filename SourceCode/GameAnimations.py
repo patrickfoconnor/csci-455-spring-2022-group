fc2f996ebaaa55b23cf5f82cd1721f811934746f
emiliabourgeois <tyler.bourgeois@student.montana.edu>
2022-05-04 10:45:06 -0600

    animations

--- SourceCode/GameAnimations.py
+++ SourceCode/GameAnimations.py
@@ -12,6 +12,12 @@ class GameAnimations:
         self.canvas = tk.Canvas(self.win, highlightthickness=0, bg="light blue", width=480, height=480)
         self.canvas.pack()
         self.createButtons()
+        self.chests = []
+        self.chests.append(tk.PhotoImage(file="Game/chest1.png"))
+        self.chests.append(tk.PhotoImage(file="Game/chest2.png"))
+        self.chests.append(tk.PhotoImage(file="Game/chest3.png"))
+        self.chests.append(tk.PhotoImage(file="Game/chest4.png"))
+        self.chests.append(tk.PhotoImage(file="Game/chest5.png"))
         self.hearts = []
         self.hearts.append(tk.PhotoImage(file="Game/heart0.png"))
         self.hearts.append(tk.PhotoImage(file="Game/heart.png"))
@@ -22,10 +28,26 @@ class GameAnimations:
         self.win.update_idletasks()
         self.win.update()
 
+    def chest(self):
+        self.canvas.create_image((250, 250), image=self.chests[0])
+        self.canvas.pack()
+        self.win.update()
+        self.win.after(2000)
+        for img in self.chests[1:]:
+            self.canvas.create_image((250,250),image=img)
+            self.canvas.pack()
+            self.win.update()
+            self.win.after(500)
+        self.win.after(3000)
+        self.canvas.delete('all')
+        self.canvas.pack()
+        self.win.update()
+
     def victory(self):
         txt = self.canvas.create_text((240,240),fill ="yellow", text="VICTORY ACHIEVED",font=("Times New Roman", 4))
         self.canvas.pack()
         self.win.update()
+        self.win.after(2000)
         for i in range(30):
             self.win.after(10)
             self.canvas.itemconfig(txt, font=("Times New Roman", 4 + (i)))
@@ -39,7 +61,11 @@ class GameAnimations:
         self.win.update()
 
     def recharge(self):
-        for img in self.hearts:
+        self.canvas.create_image((100, 250), image=self.hearts[0], anchor=tk.W)
+        self.canvas.pack()
+        self.win.update()
+        self.win.after(2000)
+        for img in self.hearts[1:]:
             self.canvas.create_image((100,250),image=img,anchor=tk.W)
             self.canvas.pack()
             self.win.update()
@@ -55,6 +81,4 @@ class GameAnimations:
 
 
 ani = GameAnimations()
-
-
-
+ani.chest()
